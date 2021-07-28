9526c545f6966c99e0b4e05208505adf13609f3d
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2021-07-28 12:36:36 +0200

    Add styles and toggle light and dark mode

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 class BookList extends Component {
+    static contextType = ThemeContext;
 
     render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return (
-            <div className="book-list">
+            <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
                 <ul>
-                    <li>"The fellowship of the ring"</li>
-                    <li>"Two Towers"</li>
-                    <li>"Return of the King"</li>
+                    <li style={{ background: theme.ui }}>"The fellowship of the ring"</li>
+                    <li style={{ background: theme.ui }}>"Two Towers"</li>
+                    <li style={{ background: theme.ui }}>"Return of the King"</li>
                 </ul>
             </div>
         );
